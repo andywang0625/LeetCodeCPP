9a0d3ccf9f26242332d4b5d1079e63d0977691ef
Kanade <andy_wang0625@hotmail.com>
2019-04-09 15:31:08 -0400

    Longest Substring Without Repeating Characters
    5%

---
+++ substring.cpp
@@ -0,0 +1,51 @@
+#include "string"
+#include "iostream"
+#include <cmath>
+using namespace std;
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int ans = 0, p=0, psub=0;
+        string sub[10000]="";
+        while(s[p] != '\0'){
+            for(int z=p; z<n;z++){
+                if(llUnique(sub[psub], s[z])){
+                    sub[psub]+=s[z];
+                }
+                else{
+                    psub++;
+                    p++;
+                    break;
+                }
+            }
+        }
+        p=0;
+        while(sub[p]!=""){
+            if(ans<sub[p].length())
+                ans=sub[p].length();
+            p++;
+        }
+        return ans;
+    }
+
+    bool llUnique(string sub, char c) {
+        if(sub!=""){
+            for (int i = 0; i < sub.length(); i++) {
+                if(sub[i]==c){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+  string a;
+  cin>>a;
+  cout<<Solution().lengthOfLongestSubstring(a);
+  return 0;
+}
